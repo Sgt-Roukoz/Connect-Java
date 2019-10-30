fbb0bd19107876237bfb38a7fb6c7d2c3ad38ae8
S347096083 <S347096083@LDHS-BYT-96915.ad.ocdsb.ca>
2019-10-30 09:50:22 -0400

    Creating Base Files for Project

---
+++ Connect-Java/src/connect/java/ConnectJava.java
@@ -0,0 +1,13 @@
+//Main ConectJava class
+
+package connect.java;
+
+public class ConnectJava {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
